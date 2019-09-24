b1a5fd5f1c1a1d1c2ec1e76d11122991fa119216
micaelamccall <mica2014@comcast.net>
2019-09-23 23:32:21 -0400

    add notebooks

--- plots/CAHPSpairplot.py
+++ plots/CAHPSpairplot.py
@@ -21,18 +21,6 @@ for i in range(3):
             d.axes[i][j].set_xlabel(replacements[xlabel])
         if ylabel in replacements.keys():
             d.axes[i][j].set_ylabel(replacements[ylabel])
-plt.close()
-
-
-sns.set_style('white')
-sns.set_style('ticks')
-a = sns.scatterplot(data = plt_df, x = "rate_of_readmission", y = "patients_rating_of_the_facility_linear_mean_score", hue = "is-MUA?",  palette=["#9b59b6", "#3498db"])
-sns.despine()
-a.xaxis.set_major_locator(ticker.MultipleLocator(1))
-plt.xlabel("Readmission Rate")
-plt.ylabel("Patients' Mean Rating of the Facility")
-custom = [Line2D([], [], marker='.', color="#9b59b6", linestyle='None'),Line2D([], [], marker='.', color="#3498db", linestyle='None')]
-plt.legend(custom, ['MUA', 'Not MUA'], loc='lower right')
 
 plt.close()
 
--- plots/MUA_scatterplots.py
+++ plots/MUA_scatterplots.py
@@ -11,6 +11,18 @@ plt_df = mergedMUA_df
 
 sns.set_style('white')
 sns.set_style('ticks')
+a = sns.scatterplot(data = plt_df, x = "rate_of_readmission", y = "patients_rating_of_the_facility_linear_mean_score", hue = "is-MUA?",  palette=["#9b59b6", "#3498db"])
+sns.despine()
+a.xaxis.set_major_locator(ticker.MultipleLocator(1))
+plt.xlabel("Readmission Rate")
+plt.ylabel("Patients' Mean Rating of the Facility")
+custom = [Line2D([], [], marker='.', color="#9b59b6", linestyle='None'),Line2D([], [], marker='.', color="#3498db", linestyle='None')]
+plt.legend(custom, ['MUA', 'Not MUA'], loc='lower right')
+
+plt.close()
+
+sns.set_style('white')
+sns.set_style('ticks')
 b = sns.scatterplot(data = plt_df, x = "rate_of_readmission", y = "patients_recommending_the_facility_linear_mean_score", hue = "is-MUA?",  palette=["#9b59b6", "#2ecc71"])
 sns.despine()
 b.xaxis.set_major_locator(ticker.MultipleLocator(1))
@@ -33,3 +45,12 @@ plt.legend(custom, ['MUA', 'Not MUA'], loc='lower right')
 
 plt.close()
 
+sns.set_style('white')
+sns.set_style('ticks')
+b = sns.scatterplot(data = plt_df, x = "rate_of_readmission", y = "patients_recommending_the_facility_linear_mean_score", hue = "MUA-population-type",  palette="muted")
+sns.despine()
+b.xaxis.set_major_locator(ticker.MultipleLocator(1))
+plt.xlabel("Readmission Rate")
+plt.ylabel("Patients' Mean Recommendation of the Facility")
+
+plt.close()
