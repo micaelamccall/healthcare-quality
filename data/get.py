0e03822f9adf4dd57d4de4f8ca7363fd7172c1f8
micaelamccall <mica2014@comcast.net>
2019-09-26 15:36:27 -0400

    add gitignore

--- data/get.py
+++ data/get.py
@@ -1,8 +1,17 @@
 import pandas as pd 
 import requests
 import json
+import settings
 
-auth = ('9asut06cuhbleteyw87dhfibv', '4bllkisduibcmxokd9r9xv0bkiwo3a2lhl87oz14yflogimhzf')
+API_TOKEN=''
+API_SECRET=''
+
+try:
+    from private import *
+except: 
+    pass
+
+auth = (API_TOKEN, API_SECRET)
 
 queries = [["https://data.medicare.gov/resource/qk5r-kh7u.json?$limit=5000", pd.DataFrame()], ["https://data.medicare.gov/resource/r32h-32z5.json?$limit=5000&$where=measure_name= 'Rate of readmission after discharge from hospital (hospital-wide)' AND score != 'Not Available'", pd.DataFrame()]]
 
